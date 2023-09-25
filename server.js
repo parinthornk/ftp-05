e180d910920baed6e8ce9c0e213a93ea72507d8c
Parinthorn Kettubtim <zparinthornk@pttep.com>
2023-09-25 19:52:23 +0700

    Added      startbootstrap-sb-admin-2/events.html Added      startbootstrap-sb-admin-2/item-details.html Added      startbootstrap-sb-admin-2/schedules-_.html Added      startbootstrap-sb-admin-2/schedules-add.html Added      startbootstrap-sb-admin-2/schedules-edit.html Added      startbootstrap-sb-admin-2/schedules-sessions.html Added      startbootstrap-sb-admin-2/sessions-_.html Added      startbootstrap-sb-admin-2/sessions-items.html Modified   server.js Modified   startbootstrap-sb-admin-2/logs-items.html Modified   startbootstrap-sb-admin-2/schedules.html Modified   startbootstrap-sb-admin-2/sites.html

--- server.js
+++ server.js
@@ -86,14 +86,14 @@ let host = require("os").hostname();
 if (host.startsWith("service-")) {
 	url_ref = "https://squid-app-o8e56.ondigitalocean.app";
 } else {
-	url_ref = "http://localhost:3000"
+	url_ref = "http://localhost:3000/v20"
 }
 
 /*function replace_url_ref (env, _m) {
 	let __m = _m;
 	if (host.startsWith("service-")) {
-		while (__m.includes("http://localhost:3000/" + env)) {
-			__m = __m.replace("http://localhost:3000/" + env, "https://squid-app-o8e56.ondigitalocean.app/" + env);
+		while (__m.includes("http://localhost:3000/v20/" + env)) {
+			__m = __m.replace("http://localhost:3000/v20/" + env, "https://squid-app-o8e56.ondigitalocean.app/" + env);
 		}
 	}
 	return __m;
@@ -102,12 +102,12 @@ if (host.startsWith("service-")) {
 function replace_url_ref (env, _m) {
 	let __m = _m;
 	if (host.startsWith("service-")) {
-		while (__m.includes("http://localhost:3000")) {
-			__m = __m.replace("http://localhost:3000", "https://squid-app-o8e56.ondigitalocean.app");
+		while (__m.includes("http://localhost:3000/v20")) {
+			__m = __m.replace("http://localhost:3000/v20", "https://squid-app-o8e56.ondigitalocean.app");
 		}
 	}
 	__m = __m.replaceAll("{replace_with_bgcolor}", env_get_backgroundColor(env));
-	return __m.replaceAll("http://localhost:3000", "http://localhost:3000/" + env).replaceAll("https://squid-app-o8e56.ondigitalocean.app", "https://squid-app-o8e56.ondigitalocean.app/" + env);
+	return __m.replaceAll("http://localhost:3000/v20", "http://localhost:3000/v20/" + env).replaceAll("https://squid-app-o8e56.ondigitalocean.app", "https://squid-app-o8e56.ondigitalocean.app/" + env);
 }
 
 // ---------------------------------------------------------------------------------------- read host end
@@ -254,7 +254,7 @@ app.all('/:env/sites', (req, response) => {
 	
 	let requested_env = req.params.env;
 	
-	let filePath = "sites.html";
+	let filePath = "startbootstrap-sb-admin-2/sites.html";
 	
 	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
 		if (!err) {
@@ -282,7 +282,7 @@ app.all('/:env/schedules', (req, response) => {
 	
 	let requested_env = req.params.env;
 	
-	let filePath = "schedules.html";
+	let filePath = "startbootstrap-sb-admin-2/schedules.html";
 	
 	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
 		if (!err) {
@@ -310,7 +310,7 @@ app.all('/:env/schedules-add', (req, response) => {
 	
 	let requested_env = req.params.env;
 	
-	let filePath = "schedules-add.html";
+	let filePath = "startbootstrap-sb-admin-2/schedules-add.html";
 	
 	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
 		if (!err) {
@@ -338,7 +338,7 @@ app.all('/:env/schedules/:schedule/edit', (req, response) => {
 	
 	let requested_env = req.params.env;
 	
-	let filePath = "schedules-edit.html";
+	let filePath = "startbootstrap-sb-admin-2/schedules-edit.html";
 	
 	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
 		if (!err) {
@@ -368,7 +368,7 @@ app.all('/:env/schedules/:schedule', (req, response) => {
 	
 	let requested_env = req.params.env;
 	
-	let filePath = "schedules-_.html";
+	let filePath = "startbootstrap-sb-admin-2/schedules-_.html";
 	
 	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
 		if (!err) {
@@ -399,7 +399,7 @@ app.all('/:env/schedules/:schedule/sessions', (req, response) => {
 	
 	let requested_env = req.params.env;
 	
-	let filePath = "schedules-sessions.html";
+	let filePath = "startbootstrap-sb-admin-2/schedules-sessions.html";
 	
 	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
 		if (!err) {
@@ -430,7 +430,7 @@ app.all('/:env/events', (req, response) => {
 	
 	let requested_env = req.params.env;
 	
-	let filePath = "events.html";
+	let filePath = "startbootstrap-sb-admin-2/events.html";
 	
 	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
 		if (!err) {
@@ -459,7 +459,7 @@ app.all('/:env/sessions/:session/items', (req, response) => {
 	
 	let requested_env = req.params.env;
 	
-	let filePath = "sessions-items.html";
+	let filePath = "startbootstrap-sb-admin-2/sessions-items.html";
 	
 	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
 		if (!err) {
@@ -490,7 +490,7 @@ app.all('/:env/sessions/:session/items/:item', (req, response) => {
 	
 	let requested_env = req.params.env;
 	
-	let filePath = "items-_.html";
+	let filePath = "startbootstrap-sb-admin-2/item-details.html";
 	
 	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
 		if (!err) {
@@ -522,7 +522,7 @@ app.all('/:env/items-summary', (req, response) => {
 	
 	let requested_env = req.params.env;
 	
-	let filePath = "items-summary.html";
+	let filePath = "startbootstrap-sb-admin-2/logs-items.html";
 	
 	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
 		if (!err) {
@@ -551,7 +551,7 @@ app.all('/:env/schedules/:schedule/sessions/:session', (req, response) => {
 	
 	let requested_env = req.params.env;
 	
-	let filePath = "sessions-_.html";
+	let filePath = "startbootstrap-sb-admin-2/sessions-_.html";
 	
 	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
 		if (!err) {
@@ -656,6 +656,81 @@ let internal_links = [
 		"url": "/v20/:env/vendor/fontawesome-free/webfonts/fa-solid-900.ttf",
 		"file": "startbootstrap-sb-admin-2/vendor/fontawesome-free/webfonts/fa-solid-900.ttf",
 		"type": "text/plain"
+	},
+	{
+		"url": "/libs/vendor/fontawesome-free/css/all.min.css",
+		"file": "startbootstrap-sb-admin-2/vendor/fontawesome-free/css/all.min.css",
+		"type": "text/css"
+	},
+	{
+		"url": "/libs/css/sb-admin-2.min.css",
+		"file": "startbootstrap-sb-admin-2/css/sb-admin-2.min.css",
+		"type": "text/css"
+	},
+	{
+		"url": "/libs/vendor/datatables/dataTables.bootstrap4.min.css",
+		"file": "startbootstrap-sb-admin-2/vendor/datatables/dataTables.bootstrap4.min.css",
+		"type": "text/css"
+	},
+	{
+		"url": "/libs/vendor/jquery/jquery.min.js",
+		"file": "startbootstrap-sb-admin-2/vendor/jquery/jquery.min.js",
+		"type": "text/javascript"
+	},
+	{
+		"url": "/libs/vendor/bootstrap/js/bootstrap.bundle.min.js",
+		"file": "startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min.js",
+		"type": "text/javascript"
+	},
+	{
+		"url": "/libs/vendor/jquery-easing/jquery.easing.min.js",
+		"file": "startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing.min.js",
+		"type": "text/javascript"
+	},
+	{
+		"url": "/libs/js/sb-admin-2.min.js",
+		"file": "startbootstrap-sb-admin-2/js/sb-admin-2.min.js",
+		"type": "text/javascript"
+	},
+	{
+		"url": "/libs/vendor/datatables/jquery.dataTables.min.js",
+		"file": "startbootstrap-sb-admin-2/vendor/datatables/jquery.dataTables.min.js",
+		"type": "text/javascript"
+	},
+	{
+		"url": "/libs/vendor/datatables/dataTables.bootstrap4.min.js",
+		"file": "startbootstrap-sb-admin-2/vendor/datatables/dataTables.bootstrap4.min.js",
+		"type": "text/javascript"
+	},
+	{
+		"url": "/libs/js/demo/datatables-demo.js",
+		"file": "startbootstrap-sb-admin-2/js/demo/datatables-demo.js",
+		"type": "text/javascript"
+	},
+	{
+		"url": "/libs/img/logo-or-white.png",
+		"file": "startbootstrap-sb-admin-2/img/logo-or-white.png",
+		"type": "image/png"
+	},
+	{
+		"url": "/libs/img/logo-wso2.png",
+		"file": "startbootstrap-sb-admin-2/img/logo-wso2.png",
+		"type": "image/png"
+	},
+	{
+		"url": "/libs/vendor/fontawesome-free/webfonts/fa-solid-900.woff2",
+		"file": "startbootstrap-sb-admin-2/vendor/fontawesome-free/webfonts/fa-solid-900.woff2",
+		"type": "text/plain"
+	},
+	{
+		"url": "/libs/vendor/fontawesome-free/webfonts/fa-solid-900.woff",
+		"file": "startbootstrap-sb-admin-2/vendor/fontawesome-free/webfonts/fa-solid-900.woff",
+		"type": "text/plain"
+	},
+	{
+		"url": "/libs/vendor/fontawesome-free/webfonts/fa-solid-900.ttf",
+		"file": "startbootstrap-sb-admin-2/vendor/fontawesome-free/webfonts/fa-solid-900.ttf",
+		"type": "text/plain"
 	}
 ];
 
@@ -733,6 +808,245 @@ app.get('/v20/:env/schedules', (req, response) => {
 			get_token(requested_env, (token) => {
 				let data_with_token = data.replace("{access_token}", token["access_token"]);
 				data_with_token = replace_url_ref(requested_env, data_with_token);
+				console.log(data_with_token);
+				response.setHeader('Content-Type', 'text/html');
+				response.status(200);
+				response.end(data_with_token);
+			}, (e) => {
+				response.writeHead(500, {'Content-Type': 'text/html'});
+				response.write(e);
+				response.end();
+			});
+		} else {
+			response.writeHead(500, {'Content-Type': 'text/html'});
+			response.write("error");
+			response.end();
+		}
+	});
+});
+
+app.get('/v20/:env/sessions/:session/items/:item', (req, response) => {
+	let requested_env = req.params.env;
+	let filePath = "startbootstrap-sb-admin-2/item-details.html";
+	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
+		if (!err) {
+			get_token(requested_env, (token) => {
+				let data_with_token = data.replace("{access_token}", token["access_token"]);
+				data_with_token = replace_url_ref(requested_env, data_with_token);
+				data_with_token = data_with_token.replace("{session}", req.params.session);
+				data_with_token = data_with_token.replace("{item}", req.params.item);
+				console.log(data_with_token);
+				response.setHeader('Content-Type', 'text/html');
+				response.status(200);
+				response.end(data_with_token);
+			}, (e) => {
+				response.writeHead(500, {'Content-Type': 'text/html'});
+				response.write(e);
+				response.end();
+			});
+		} else {
+			response.writeHead(500, {'Content-Type': 'text/html'});
+			response.write("error");
+			response.end();
+		}
+	});
+});
+
+app.all('/v20/:env/schedules/:schedule', (req, response) => {
+	
+	let requested_env = req.params.env;
+	
+	let filePath = "startbootstrap-sb-admin-2/schedules-_.html";
+	
+	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
+		if (!err) {
+			get_token(requested_env, (token) => {
+				let data_with_token = data.replace("{access_token}", token["access_token"]);
+				data_with_token = replace_url_ref(requested_env, data_with_token);
+				data_with_token = data_with_token.replace("{schedule}", req.params.schedule);
+				
+				
+				console.log(data_with_token);
+				response.setHeader('Content-Type', 'text/html');
+				response.status(200);
+				response.end(data_with_token);
+			}, (e) => {
+				response.writeHead(500, {'Content-Type': 'text/html'});
+				response.write(e);
+				response.end();
+			});
+		} else {
+			response.writeHead(500, {'Content-Type': 'text/html'});
+			response.write("error");
+			response.end();
+		}
+	});
+});
+
+app.all('/v20/:env/schedules/:schedule/sessions', (req, response) => {
+	
+	let requested_env = req.params.env;
+	
+	let filePath = "startbootstrap-sb-admin-2/schedules-sessions.html";
+	
+	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
+		if (!err) {
+			get_token(requested_env, (token) => {
+				let data_with_token = data.replace("{access_token}", token["access_token"]);
+				data_with_token = replace_url_ref(requested_env, data_with_token);
+				data_with_token = data_with_token.replace("{schedule}", req.params.schedule);
+				
+				
+				console.log(data_with_token);
+				response.setHeader('Content-Type', 'text/html');
+				response.status(200);
+				response.end(data_with_token);
+			}, (e) => {
+				response.writeHead(500, {'Content-Type': 'text/html'});
+				response.write(e);
+				response.end();
+			});
+		} else {
+			response.writeHead(500, {'Content-Type': 'text/html'});
+			response.write("error");
+			response.end();
+		}
+	});
+});
+
+app.all('/v20/:env/sessions/:session/items', (req, response) => {
+	
+	let requested_env = req.params.env;
+	
+	let filePath = "startbootstrap-sb-admin-2/sessions-items.html";
+	
+	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
+		if (!err) {
+			get_token(requested_env, (token) => {
+				let data_with_token = data.replace("{access_token}", token["access_token"]);
+				data_with_token = replace_url_ref(requested_env, data_with_token);
+				data_with_token = data_with_token.replace("{session}", req.params.session);
+				
+				
+				console.log(data_with_token);
+				response.setHeader('Content-Type', 'text/html');
+				response.status(200);
+				response.end(data_with_token);
+			}, (e) => {
+				response.writeHead(500, {'Content-Type': 'text/html'});
+				response.write(e);
+				response.end();
+			});
+		} else {
+			response.writeHead(500, {'Content-Type': 'text/html'});
+			response.write("error");
+			response.end();
+		}
+	});
+});
+
+app.all('/v20/:env/schedules-add', (req, response) => {
+	
+	let requested_env = req.params.env;
+	
+	let filePath = "startbootstrap-sb-admin-2/schedules-add.html";
+	
+	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
+		if (!err) {
+			get_token(requested_env, (token) => {
+				let data_with_token = data.replace("{access_token}", token["access_token"]);
+				data_with_token = replace_url_ref(requested_env, data_with_token);
+				console.log(data_with_token);
+				response.setHeader('Content-Type', 'text/html');
+				response.status(200);
+				response.end(data_with_token);
+			}, (e) => {
+				response.writeHead(500, {'Content-Type': 'text/html'});
+				response.write(e);
+				response.end();
+			});
+		} else {
+			response.writeHead(500, {'Content-Type': 'text/html'});
+			response.write("error");
+			response.end();
+		}
+	});
+});
+
+app.all('/v20/:env/schedules/:schedule/edit', (req, response) => {
+	
+	let requested_env = req.params.env;
+	
+	let filePath = "startbootstrap-sb-admin-2/schedules-edit.html";
+	
+	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
+		if (!err) {
+			get_token(requested_env, (token) => {
+				let data_with_token = data.replace("{access_token}", token["access_token"]);
+				data_with_token = replace_url_ref(requested_env, data_with_token);
+				data_with_token = data_with_token.replace("{schedule}", req.params.schedule);
+				
+				console.log(data_with_token);
+				response.setHeader('Content-Type', 'text/html');
+				response.status(200);
+				response.end(data_with_token);
+			}, (e) => {
+				response.writeHead(500, {'Content-Type': 'text/html'});
+				response.write(e);
+				response.end();
+			});
+		} else {
+			response.writeHead(500, {'Content-Type': 'text/html'});
+			response.write("error");
+			response.end();
+		}
+	});
+});
+
+app.all('/v20/:env/schedules/:schedule/sessions/:session', (req, response) => {
+	
+	let requested_env = req.params.env;
+	
+	let filePath = "startbootstrap-sb-admin-2/sessions-_.html";
+	
+	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
+		if (!err) {
+			get_token(requested_env, (token) => {
+				let data_with_token = data.replace("{access_token}", token["access_token"]);
+				data_with_token = replace_url_ref(requested_env, data_with_token);
+				data_with_token = data_with_token.replace("{schedule}", req.params.schedule);
+				data_with_token = data_with_token.replace("{session}", req.params.session);
+				
+				
+				console.log(data_with_token);
+				response.setHeader('Content-Type', 'text/html');
+				response.status(200);
+				response.end(data_with_token);
+			}, (e) => {
+				response.writeHead(500, {'Content-Type': 'text/html'});
+				response.write(e);
+				response.end();
+			});
+		} else {
+			response.writeHead(500, {'Content-Type': 'text/html'});
+			response.write("error");
+			response.end();
+		}
+	});
+});
+
+app.all('/v20/:env/events', (req, response) => {
+	
+	let requested_env = req.params.env;
+	
+	let filePath = "startbootstrap-sb-admin-2/events.html";
+	
+	fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data){
+		if (!err) {
+			get_token(requested_env, (token) => {
+				let data_with_token = data.replace("{access_token}", token["access_token"]);
+				data_with_token = replace_url_ref(requested_env, data_with_token);
+				
 				console.log(data_with_token);
 				response.setHeader('Content-Type', 'text/html');
 				response.status(200);
